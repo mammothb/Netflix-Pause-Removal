1fc91186335e017af6cf4112718a3b50546464b9
mmb L <mammothb+git@gmail.com>
2017-09-25 21:25:50 +0800

    added match urls and added semicolon

--- Netflix-Pause-Removal.user.js
+++ Netflix-Pause-Removal.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name        Netflix Pause Removal
 // @namespace   *://www.netflix.com
-// @version     1.1.0
+// @match       *://www.netflix.com/watch/*
+// @version     1.2.0
 // @grant       none
 // @description Automatically clicks "continue playing"
 // ==/UserScript==
 setInterval(function(){}, 9999); //Dummy SetInterval (Gets cleared during netflix's load events)
-setInterval(function(){try{document.getElementsByClassName("button continue-playing")[0].click()}catch(e){}}, 33);
+setInterval(function(){try {document.getElementsByClassName("button continue-playing")[0].click();}catch(e){}}, 33);
