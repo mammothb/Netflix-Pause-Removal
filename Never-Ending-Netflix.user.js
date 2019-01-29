5c26db0fbc50cb62498a41e94a2b5356b32c13a0
mmb L <mammothb+git@gmail.com>
2019-01-29 11:56:14 +0800

    removed dummy interval, used if intead of try...except

--- Never-Ending-Netflix.user.js
+++ Never-Ending-Netflix.user.js
@@ -3,24 +3,20 @@
 // @namespace   *://www.netflix.com
 // @include     http://www.netflix.com/*
 // @include     https://www.netflix.com/*
-// @version     1.5.0
+// @version     1.6.0
 // @grant       none
 // @description Automatically clicks "Continue Playing", "Skip Intro", and "Play Next Episode"
 // ==/UserScript==
 
-// Dummy SetInterval (Gets cleared during netflix's load events)
-setInterval(function() {}, 9999);
+/**
+ * Click the button if it is found
+ */
+function clickIfFound(button) {
+  if (button) button.click();
+}
+
 setInterval(function() {
-  try {
-    document.querySelector("[aria-label='Continue Playing']").click();
-  }
-  catch(e) {}
-  try {
-    document.querySelector("[aria-label='Skip Intro']").click();
-  }
-  catch(e) {}
-  try {
-    document.querySelector("[class='WatchNext-still-hover-container']").click();
-  }
-  catch(e) {}
+  clickIfFound(document.querySelector("[aria-label='Skip Intro']"));
+  clickIfFound(document.querySelector("[aria-label='Continue Playing']"));
+  clickIfFound(document.querySelector("[class='WatchNext-still-hover-container']"));
 }, 33);
