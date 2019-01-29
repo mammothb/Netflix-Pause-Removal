5dda65a91f566d43ee1c095fa17946e2a4695015
mmb L <mammothb+git@gmail.com>
2019-01-29 13:07:23 +0800

    Delete background.js

--- background.js
+++
@@ -1,2 +0,0 @@
-setInterval(function(){}, 9999); //Dummy SetInterval (Gets cleared during netflix's load events)
-setInterval(function(){try{document.getElementsByClassName("button continue-playing")[0].click()}catch(e){}}, 33);
