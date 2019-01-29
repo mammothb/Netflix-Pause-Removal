ae3d200be9d02e9ab21cdc6c204d665bfde53857
mmb L <mammothb+git@gmail.com>
2019-01-29 15:37:49 +0800

    added functions to seek 1min and 5min

--- Never-Ending-Netflix.user.js
+++ Never-Ending-Netflix.user.js
@@ -15,8 +15,95 @@ function clickIfFound(button) {
   if (button) button.click();
 }
 
-setInterval(function() {
+let netflixSeeker = function () {
+  // used when the user is spamming ctrl/shift+arrow
+  let seekTime = {
+    "from": null,
+    "to": null
+  };
+
+  function showControls() {
+    let scrubber = document.querySelector(".scrubber-bar");
+    let eventOptions = {
+      "bubbles": true,
+      "button": 0,
+      "currentTarget": scrubber
+    };
+    scrubber.dispatchEvent(new MouseEvent("mousemove", eventOptions));
+  };
+
+  function getCurrentTime() {
+    return document.querySelector("video").currentTime;
+  };
+
+  function getDuration() {
+    return document.querySelector("video").duration;
+  };
+
+  function seek(seconds) {
+    seconds = Math.min(Math.max(seconds, 0), getDuration());
+    console.log("seek from: ", getCurrentTime(), "to: ", seconds);
+    showControls();
+
+    let scrubber = document.querySelector(".scrubber-bar");
+    let mouseX = Math.round(scrubber.getBoundingClientRect().x + scrubber.clientWidth * seconds / getDuration());
+    let mouseY = Math.round(scrubber.getBoundingClientRect().y + scrubber.clientHeight / 2);
+    let eventOptions = {
+      "bubbles": true,
+      "button": 0,
+      "clientX": mouseX,
+      "clientY": mouseY
+    };
+    scrubber.dispatchEvent(new MouseEvent("mouseover", eventOptions));
+    window.setTimeout(function () {
+      // simulate a click on the scrubber
+      scrubber.dispatchEvent(new MouseEvent("mousedown", eventOptions));
+      scrubber.dispatchEvent(new MouseEvent("mouseup", eventOptions));
+      scrubber.dispatchEvent(new MouseEvent("mouseout", eventOptions));
+    }, 10);
+  }
+
+  function incrementSeek(seconds) {
+    let currentTime = getCurrentTime();
+    let toTime = currentTime + seconds;
+    if (currentTime === seekTime.from) {
+      // user is probably spamming seek and the player hasn't had a chance to update yet
+      toTime = seekTime.to + seconds;
+    }
+    seekTime = {
+      "from": currentTime,
+      "to": toTime
+    };
+    seek(toTime);
+  }
+
+  return {
+    incrementSeek: incrementSeek
+  }
+}();
+
+setInterval(function () {
   clickIfFound(document.querySelector("[aria-label='Skip Intro']"));
   clickIfFound(document.querySelector("[aria-label='Continue Playing']"));
   clickIfFound(document.querySelector("[class='WatchNext-still-hover-container']"));
 }, 33);
+
+document.querySelector("body").addEventListener("keydown", function (e) {
+  // Ctrl+arrow to skip 60sec and Shift+arrow to skip 5 mins
+  let found = true;
+  if (e.code === "ArrowRight" && e.shiftKey) {
+    netflixSeeker.incrementSeek(5 * 60);
+  } else if (e.code === "ArrowRight" && e.ctrlKey) {
+    netflixSeeker.incrementSeek(60);
+  } else if (e.code === "ArrowLeft" && e.shiftKey) {
+    netflixSeeker.incrementSeek(-5 * 60);
+  } else if (e.code === "ArrowLeft" && e.ctrlKey) {
+    netflixSeeker.incrementSeek(-60);
+  } else {
+    found = false;
+  }
+  if (found) {
+    e.stopPropagation();
+    return false;
+  }
+});
