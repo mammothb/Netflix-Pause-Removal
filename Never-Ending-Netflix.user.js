724b4af485c9a474ddd8990ac55446385cb293f0
mmb <mammothb+git@gmail.com>
2019-02-11 16:35:19 +0800

    add one more watch next option

--- Never-Ending-Netflix.user.js
+++ Never-Ending-Netflix.user.js
@@ -16,10 +16,17 @@ function clickIfFound(button) {
   if (button) button.click();
 }
 
-/**
- * Contains functions to get video duration and dimensions of the
- * scrubber-bar. Contains functions to increment seek 1min or 5min.
- */
+setInterval(function () {
+  clickIfFound(document.querySelector("[aria-label='Skip Intro']"));
+  clickIfFound(document.querySelector("[aria-label='Continue Playing']"));
+  clickIfFound(document.querySelector(
+      "[class='WatchNext-still-hover-container']"));
+  clickIfFound(document.querySelector("[aria-label^='Next episode in']"));
+}, 33);
+
+/*********************************************************************
+ *            Implement additional seek functionality
+ *********************************************************************/
 let netflixSeeker = function () {
   // used when the user is spamming ctrl/shift+arrow
   let seekTime = {
@@ -27,14 +34,12 @@ let netflixSeeker = function () {
     "to": null
   };
 
-  /**
-   * Shows the scrubber-bar by moving the mouse on the screen.
-   */
   function showControls() {
-    let scrubber = document.querySelector(".scrubber-bar");
-    let eventOptions = {
+    const scrubber = document.querySelector(".scrubber-bar");
+    const eventOptions = {
       "bubbles": true,
-      "button": 0
+      "button": 0,
+      "currentTarget": scrubber
     };
     scrubber.dispatchEvent(new MouseEvent("mousemove", eventOptions));
   };
@@ -52,12 +57,12 @@ let netflixSeeker = function () {
     console.log("seek from: ", getCurrentTime(), "to: ", seconds);
     showControls();
 
-    let scrubber = document.querySelector(".scrubber-bar");
-    let mouseX = Math.round(scrubber.getBoundingClientRect().x +
-                            scrubber.clientWidth * seconds / getDuration());
-    let mouseY = Math.round(scrubber.getBoundingClientRect().y +
-                            scrubber.clientHeight / 2);
-    let eventOptions = {
+    const scrubber = document.querySelector(".scrubber-bar");
+    const mouseX = Math.round(scrubber.getBoundingClientRect().x +
+        scrubber.clientWidth * seconds / getDuration());
+    const mouseY = Math.round(scrubber.getBoundingClientRect().y +
+        scrubber.clientHeight / 2);
+    const eventOptions = {
       "bubbles": true,
       "button": 0,
       "clientX": mouseX,
@@ -73,12 +78,11 @@ let netflixSeeker = function () {
   }
 
   function incrementSeek(seconds) {
-    let currentTime = getCurrentTime();
+    const currentTime = getCurrentTime();
     let toTime = currentTime + seconds;
-
-    // user is probably spamming seek and the player hasn't had a
-    // chance to update yet
     if (currentTime === seekTime.from) {
+      // user is probably spamming seek and the player hasn't had a
+      // chance to update yet
       toTime = seekTime.to + seconds;
     }
     seekTime = {
@@ -88,16 +92,26 @@ let netflixSeeker = function () {
     seek(toTime);
   }
 
+  function goToNext() {
+    const nextEpisode = document.querySelector(
+        "[aria-label='Next Episode']");
+    if (nextEpisode) {
+      nextEpisode.click();
+    } else {
+      seek(getDuration());
+    }
+  }
+
+  function goToStart() {
+    seek(-getDuration());
+  }
+
   return {
-    incrementSeek: incrementSeek
+    incrementSeek: incrementSeek,
+    goToNext: goToNext,
+    goToStart: goToStart
   }
 }();
-
-setInterval(function () {
-  clickIfFound(document.querySelector("[aria-label='Skip Intro']"));
-  clickIfFound(document.querySelector("[aria-label='Continue Playing']"));
-  clickIfFound(document.querySelector("[class='WatchNext-still-hover-container']"));
-}, 33);
 
 document.querySelector("body").addEventListener("keydown", function (e) {
   // Ctrl+arrow to skip 60sec and Shift+arrow to skip 5 mins
@@ -110,6 +124,10 @@ document.querySelector("body").addEventListener("keydown", function (e) {
     netflixSeeker.incrementSeek(-5 * 60);
   } else if (e.code === "ArrowLeft" && e.ctrlKey) {
     netflixSeeker.incrementSeek(-60);
+  } else if (e.code === "KeyN") {
+    netflixSeeker.goToNext();
+  } else if (e.code === "KeyP") {
+    netflixSeeker.goToStart();
   } else {
     found = false;
   }
